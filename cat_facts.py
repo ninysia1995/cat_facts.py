1e2bed6cbbd64bc8712beb5db775b42a1dd1e7a3
ninysia <125797362+ninysia1995@users.noreply.github.com>
2023-04-14 17:49:56 +0300

    Create cat_facts.py

---
+++ cat_facts.py
@@ -0,0 +1,11 @@
+import random
+
+cat_facts = [
+    "Cats have five toes on their front paws, but only four toes on their back paws.",
+    "Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.",
+    "A cat's hearing is better than a dog's. And a cat can hear high-frequency sounds up to two octaves higher than a human.",
+    "Cats spend a large amount of time licking their coats to keep them clean.",
+    "The average cat can jump up to six times its body length in a single bound.",
+]
+
+print(random.choice(cat_facts))
